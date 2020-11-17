9ba8a1f4bfd573802957feaae6f49cc9bb610c11
Otee Ziregbe <otee.ziregbe@cyberspace.net.ng>
2020-11-17 22:05:43 +0100

    initialCommit

---
+++ BootstrapPaging/BootstrapPaging/Models/PageClickModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootstrapPaging.Models
+{
+    public class PageClickModel
+    {
+        public int PageNumber { get; set; }
+
+        public bool ChangePager { get; set; }
+    }
+}
---
+++ BootstrapPaging/BootstrapPaging/PagerComponent.cs
@@ -0,0 +1,59 @@
+﻿using BootstrapPaging.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapPaging
+{
+    public partial class PagerComponent
+    {
+        [Parameter]
+        public int TotalSizeParam { get; set; }
+
+        [Parameter]
+        public int PageNumberParam { get; set; }
+
+        [Parameter]
+        public int PageSizeParam { get; set; }
+        [Parameter]
+        public bool PagerEnableNext { get; set; }
+        [Parameter]
+        public int PagerEndIndex { get; set; }
+        [Parameter]
+        public int PagerStartIndex { get; set; }
+        [Parameter]
+        public bool PagerEnablePrevious { get; set; }
+
+        [Parameter]
+        public int CurrentIndex { get; set; }
+
+
+        [Parameter]
+        public EventCallback<PageClickModel> OnPageCallBack { get; set; }
+
+
+
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
+
+
+
+
+
+        private async Task LoadData(int pageNumber, bool changePager)
+        {
+
+            await OnPageCallBack.InvokeAsync(new PageClickModel
+            {
+                PageNumber = pageNumber,
+                ChangePager = changePager
+            });
+        }
+
+
+    }
+}
