ed027a95b67a24a3efb9944b40c86e7d9cad6948
Otee Ziregbe <otee.ziregbe@cyberspace.net.ng>
2020-11-17 22:16:28 +0100

    PagerParams

---
+++ BootstrapPaging/BootstrapPaging/Models/PagerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootstrapPaging.Models
+{
+    public class PagerModel
+    {
+        public int PagerStartIndex { get; set; }
+        public int PagerEndIndex { get; set; }
+
+        public int CurrentIndex { get; set; }
+
+        public bool NextEnabled { get; set; }
+        public bool PreviousEnabled { get; set; }
+
+        public bool RefreshPager { get; set; } = true;
+    }
+
+}
---
+++ BootstrapPaging/BootstrapPaging/Util/PagerUtil.cs
@@ -0,0 +1,64 @@
+﻿using BootstrapPaging.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootstrapPaging.Util
+{
+    public class PagerUtil
+    {
+        public static PagerModel GetPagerParams(int TotalSize, int PageSize, int PageNumber, int MaximumPages)
+        {
+            int startIndex = 0;
+
+            int endIndex = 0;
+
+            int totalPages = 0;
+
+            int currentIndex = PageNumber - 1;
+
+            if (TotalSize > 0)
+            {
+                totalPages = (TotalSize / PageSize) + ((TotalSize % PageSize) > 1 ? 1 : 0);
+
+                if (totalPages <= MaximumPages)
+                {
+                    startIndex = 0;
+                    endIndex = totalPages - 1;
+                }
+                else
+                {
+                    //  XXX + YYY = MaximumPages;
+                    if (PageNumber > MaximumPages)
+                    {
+                        startIndex = currentIndex - MaximumPages + 1;
+
+                        endIndex = currentIndex;
+                    }
+                    else
+                    {
+                        startIndex = 0;
+
+                        endIndex = MaximumPages - 1;
+                    }
+
+
+                }
+
+            }
+
+
+            return new PagerModel
+            {
+                CurrentIndex = currentIndex,
+                PagerStartIndex = startIndex,
+                PagerEndIndex = endIndex,
+                NextEnabled = endIndex + 1 < totalPages,
+                PreviousEnabled = startIndex > 0,
+
+            };
+
+        }
+
+    }
+}
